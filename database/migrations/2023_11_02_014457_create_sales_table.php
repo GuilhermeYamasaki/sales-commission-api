50f57304c0c6ddf7933533eaae688a16e93c150d
Guilherme Yamasaki <guilhermeyyamasaki1@gmail.com>
2023-11-02 16:14:19 -0300

    feat: add migration of sales

---
+++ database/migrations/2023_11_02_014457_create_sales_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('seller_id')->constrained('sellers');
+            $table->decimal('sale_value', 10, 2);
+            $table->timestamp('sale_at');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
