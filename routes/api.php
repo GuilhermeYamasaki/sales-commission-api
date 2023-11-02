b4207cf9ba1433101bd6f9ceb04b2d4aff149ea0
Guilherme Yamasaki <guilhermeyyamasaki1@gmail.com>
2023-11-02 16:17:15 -0300

    feat: add endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\SaleController;
 use App\Http\Controllers\Api\SellerController;
+use App\Http\Controllers\Api\SellerSaleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,5 +22,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('sellers')->group(function () {
-    Route::post('/', [SellerController::class, 'store']);
+    Route::get('/', [SellerController::class, 'index'])->name('sellers.index');
+    Route::delete('/{id}', [SellerController::class, 'delete'])->name('sellers.delete');
+    Route::post('/', [SellerController::class, 'store'])->name('sellers.store'); //teste feito
+    Route::get('/{id}/sales', [SellerSaleController::class, 'show'])->name('sellers.sales.show');
+
+});
+
+Route::prefix('sales')->group(function () {
+    Route::get('/', [SaleController::class, 'index'])->name('sales.index');
+    // Route::get('/{id}', [SaleController::class, 'show'])->name('sales.show');
+    Route::post('/', [SaleController::class, 'store'])->name('sales.store');
 });
