9807cdd2e81c868bb09d9f2b760e9591cf995077
Guilherme Yamasaki <guilhermeyyamasaki1@gmail.com>
2023-11-03 15:53:03 -0300

    feat: improve integration test

--- tests/Feature/SellerServiceControllerTest.php
+++ tests/Feature/SellerServiceControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
 use Tests\TestCase;
 
 class SellerServiceControllerTest extends TestCase
@@ -34,15 +35,39 @@ class SellerServiceControllerTest extends TestCase
         ];
 
         // Act
-        $this->postJson(
-            route('api.sellers.store'),
-            $data,
-            [
-                'Authorization' => 'Bearer '.$this->token->plainTextToken,
-            ]
-        );
+        $this
+            ->postJson(
+                route('api.sellers.store'),
+                $data,
+                [
+                    'Authorization' => 'Bearer '.$this->token->plainTextToken,
+                ]
+            )
+            ->assertStatus(Response::HTTP_CREATED)
+            ->assertJsonStructure([
+                'message',
+            ]);
 
         // Assert
         $this->assertDatabaseHas('sellers', $data);
+    }
+
+    public function test_must_unathorized_request_without_token(): void
+    {
+        // Arrange
+        $data = [
+            'name' => fake()->name(),
+            'email' => fake()->email(),
+        ];
+
+        // Act
+        $this
+            ->postJson(
+                route('api.sellers.store'),
+                $data,
+                [
+                ]
+            )
+            ->assertStatus(Response::HTTP_UNAUTHORIZED);
     }
 }
