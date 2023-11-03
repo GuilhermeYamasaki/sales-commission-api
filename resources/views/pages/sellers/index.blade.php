a19491e6d18408c41b9292b18f8bd8d2255b13d8
Guilherme Yamasaki <guilhermeyyamasaki1@gmail.com>
2023-11-03 14:22:41 -0300

    fix: add button to back home

--- resources/views/pages/sellers/index.blade.php
+++ resources/views/pages/sellers/index.blade.php
@@ -4,7 +4,7 @@
     <div class="max-w-screen-lg mx-auto">
         <h1 class="text-2xl font-semibold mb-4">Tabela de Vendedores</h1>
         <input type="hidden" value="{{ route('api.sellers.index') }}" id="route">
-        <table class="w-full border-collapse border bg-white">
+        <table class="mb-8 w-full border-collapse border bg-white">
             <thead>
                 <tr>
                     <th class="border bg-gray-200 px-4 py-2">#</th>
@@ -16,6 +16,9 @@
             <tbody id="sellers-list">
             </tbody>
         </table>
+        <a href="{{ route('home.index') }}" class="bg-gray-500 text-white text-lg px-4 py-2 rounded-lg hover:bg-gray-600">
+            Voltar
+        </a>
     </div>
 @endsection
 
