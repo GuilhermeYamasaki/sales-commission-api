7eceed625ad913ccd5fdac5c3e2ce8d0ec53d947
Guilherme Yamasaki <guilhermeyyamasaki1@gmail.com>
2023-11-03 07:46:32 -0300

    feat: add cache in large responses

--- app/Services/SaleService.php
+++ app/Services/SaleService.php
@@ -6,15 +6,17 @@ use App\Repositories\Interfaces\SaleRepositoryInterface;
 use App\Repositories\Interfaces\SellerRepositoryInterface;
 use App\Services\Interfaces\SaleServiceInterface;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use RuntimeException;
 
 class SaleService implements SaleServiceInterface
 {
+    public const CACHE_ALL_SALES = 'api-all-sales';
+
     public function __construct(
         private readonly SaleRepositoryInterface $saleRepository,
         private readonly SellerRepositoryInterface $sellerRepository
     ) {
-
     }
 
     public function register(array $data): void
@@ -26,15 +28,27 @@ class SaleService implements SaleServiceInterface
         }
 
         $this->saleRepository->register($data);
+
+        $this->clearCache($data['seller_id']);
     }
 
     public function getAllSales(): Collection
     {
-        return $this->saleRepository->getAllSales();
+        return Cache::remember(self::CACHE_ALL_SALES, 60 * 20, function () {
+            return $this->saleRepository->getAllSales();
+        });
     }
 
     public function getSalesBySeller(int $sellerId): Collection
     {
-        return $this->saleRepository->getSalesBySeller($sellerId);
+        return Cache::remember(self::CACHE_ALL_SALES."seller-{$sellerId}", 60 * 20, function () use ($sellerId) {
+            return $this->saleRepository->getSalesBySeller($sellerId);
+        });
+    }
+
+    private function clearCache(int $sellerId): void
+    {
+        Cache::forget(self::CACHE_ALL_SALES);
+        Cache::forget(self::CACHE_ALL_SALES."seller-{$sellerId}");
     }
 }
--- app/Services/SellerService.php
+++ app/Services/SellerService.php
@@ -5,14 +5,16 @@ namespace App\Services;
 use App\Repositories\Interfaces\SellerRepositoryInterface;
 use App\Services\Interfaces\SellerServiceInterface;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use RuntimeException;
 
 class SellerService implements SellerServiceInterface
 {
+    public const CACHE_ALL_SELLERS = 'api-all-sellers';
+
     public function __construct(
         private readonly SellerRepositoryInterface $sellerRepository
     ) {
-
     }
 
     public function register(array $data): void
@@ -24,16 +26,21 @@ class SellerService implements SellerServiceInterface
         }
 
         $this->sellerRepository->register($data);
+
+        Cache::forget(self::CACHE_ALL_SELLERS);
     }
 
     public function getAllSellers(): Collection
     {
-        return $this->sellerRepository->getAllSellers()->makeHidden(['created_at', 'updated_at', 'deleted_at']);
+        return Cache::remember(self::CACHE_ALL_SELLERS, 60 * 20, function () {
+            return $this->sellerRepository->getAllSellers();
+        });
     }
 
     public function deleteSeller(int $id): void
     {
         $this->sellerRepository->deleteSeller($id);
+        Cache::forget(self::CACHE_ALL_SELLERS);
     }
 
     public function updateSellerData(array $data): void
@@ -45,5 +52,7 @@ class SellerService implements SellerServiceInterface
         }
 
         $this->sellerRepository->updateSellerData($data);
+
+        Cache::forget(self::CACHE_ALL_SELLERS);
     }
 }
