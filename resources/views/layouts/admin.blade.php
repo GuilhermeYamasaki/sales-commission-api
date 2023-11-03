111f2a9ef06e6a1ad7cdf0fb65d08c945ff29b9e
Guilherme Yamasaki <guilhermeyyamasaki1@gmail.com>
2023-11-03 11:47:14 -0300

    feat: add home page

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard - Vendas</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    @yield('content')
+    <div id="scripts">
+        @vite('resources/js/app.js')
+        @stack('scripts')
+    </div>
+</body>
+
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="w-full p-6 bg-white rounded-lg shadow-lg">
+        <h1 class="text-2xl font-bold mb-4">Selecione uma opção:</h1>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div class="p-4 bg-blue-200 rounded-lg">
+                <div class="text-xl font-semibold mb-2">Vendedores</div>
+                <p>Visualize informações sobre os vendedores.</p>
+                <a href="#" class="mt-4 block text-blue-600 hover:underline">Ver Vendedores</a>
+            </div>
+
+            <div class="p-4 bg-green-200 rounded-lg">
+                <div class="text-xl font-semibold mb-2">Vendas</div>
+                <p>Veja os detalhes das vendas realizadas.</p>
+                <a href="#" class="mt-4 block text-green-600 hover:underline">Ver Vendas</a>
+            </div>
+        </div>
+    </div>
+@endsection
